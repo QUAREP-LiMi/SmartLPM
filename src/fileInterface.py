724dd54bed5c8002eaaa63ca68c646f4813aec2e
NasserDarwish <32299062+NasserDarwish@users.noreply.github.com>
2025-05-20 20:19:08 +0200

    Add files via upload

--- src/fileInterface.py
+++ src/fileInterface.py
@@ -1,6 +1,4 @@
 """    
-@author: Nasser Darwish, Institute of Science and Technology Austria
-
 This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
